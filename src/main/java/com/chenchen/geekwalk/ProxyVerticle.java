29d4b322e8d9dbd515c7552221d9c08140201999
cc13446 <1344613920@qq.com>
2021-10-07 19:15:14 +0800

    反向代理核心的小优化：send方法的使用

--- src/main/java/com/chenchen/geekwalk/ProxyVerticle.java
+++ src/main/java/com/chenchen/geekwalk/ProxyVerticle.java
@@ -1,11 +1,7 @@
 package com.chenchen.geekwalk;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.*;
-
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class ProxyVerticle extends AbstractVerticle {
 
@@ -23,7 +19,6 @@ public class ProxyVerticle extends AbstractVerticle {
     server.requestHandler(request -> {
       // 把request暂停
       request.pause();
-
       // 获取到response
       HttpServerResponse response = request.response();
       response.setChunked(true);
@@ -34,31 +29,19 @@ public class ProxyVerticle extends AbstractVerticle {
         if (ar.succeeded()) {
           HttpClientRequest requestInner = ar.result();
           requestInner.setChunked(true);
+
           // 传递request的header
-          request.headers().forEach(entry -> {
-            if (entry.getKey().equals("Content-Type")) {
-              requestInner.putHeader(entry.getKey(), entry.getValue());
-            }
+          requestInner.headers().setAll(request.headers());
+
+          // 发送requestInner,内容为request
+          requestInner.send(request).onSuccess(responseInner -> {
+            // 传递response
+            response.headers().setAll(responseInner.headers());
+            response.send(responseInner);
+          }).onFailure(err -> {
+            err.printStackTrace();
+            response.setStatusCode(500).end(err.getMessage());
           });
-          // 发送requestInner
-          requestInner.response(arInner -> {
-            if (arInner.succeeded()) {
-              // 收到responseInner
-              HttpClientResponse responseInner = arInner.result();
-              // 传递response
-              response.setStatusCode(responseInner.statusCode());
-              responseInner.handler(response::write);
-              responseInner.endHandler(x -> response.end());
-            } else {
-              // 发送requestInner失败
-              ar.cause().printStackTrace();
-              response.setStatusCode(500).end(ar.cause().getMessage());
-            }
-          });
-          // 恢复request
-          request.resume();
-          request.handler(requestInner::write);
-          request.endHandler(x -> requestInner.end());
         } else {
           // 创建requestInner失败
           ar.cause().printStackTrace();
